75e91b7d89c96fa14083c7504a50afdfa4842c4c
Kohei Noda <ponserviceregist@gmail.com>
2025-07-23 16:34:45 +0900

    main.cpp: add const

--- main.cpp
+++ main.cpp
@@ -38,7 +38,8 @@ struct Position2D {
 auto line(const Position2D pos1, const Position2D pos2, TGAImage& image, const TGAColor& color) -> void {
     auto start = pos1;
     auto end   = pos2;
-    auto steep = std::abs(start.x - end.x) < std::abs(start.y - end.y);
+
+    const auto steep = std::abs(start.x - end.x) < std::abs(start.y - end.y);
     if(steep) {
         std::swap(start.x, start.y);
         std::swap(end.x, end.y);
@@ -49,8 +50,8 @@ auto line(const Position2D pos1, const Position2D pos2, TGAImage& image, const T
     auto y      = start.y;
     auto ierror = 0;
     for(auto x = start.x; x <= end.x; x++) {
-        auto realx = steep ? y : x;
-        auto realy = steep ? x : y;
+        const auto realx = steep ? y : x;
+        const auto realy = steep ? x : y;
         image.set(realx, realy, color);
         ierror += 2 * std::abs(end.y - start.y);
         if(ierror > end.x - start.x) {
@@ -60,15 +61,15 @@ auto line(const Position2D pos1, const Position2D pos2, TGAImage& image, const T
     }
 }
 
-auto project(vec3 v) -> Position2D {
+auto project(const vec3 v) -> Position2D {
     return Position2D{int((v.x + 1) * width / 2), int((v.y + 1) * height / 2)};
 }
 
-auto signed_triangle_area(Position2D a, Position2D b, Position2D c) -> double {
+auto signed_triangle_area(const Position2D a, const Position2D b, const Position2D c) -> double {
     return 0.5 * ((b.y - a.y) * (b.x + a.x) + (c.y - b.y) * (c.x + b.x) + (a.y - c.y) * (a.x + c.x));
 }
 
-auto triangle(std::array<Position2D, 3> t, TGAImage& image, const TGAColor& color) -> void {
+auto triangle(const std::array<Position2D, 3> t, TGAImage& image, const TGAColor& color) -> void {
     const auto boundary_box_min_x = std::min(std::min(t[0].x, t[1].x), t[2].x);
     const auto boundary_box_min_y = std::min(std::min(t[0].y, t[1].y), t[2].y);
     const auto boundary_box_max_x = std::max(std::max(t[0].x, t[1].x), t[2].x);
