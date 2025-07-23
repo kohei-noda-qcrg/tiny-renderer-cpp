90875d0e64e523b0bcbaab788159a82706e1835a
Kohei Noda <ponserviceregist@gmail.com>
2025-07-23 16:30:52 +0900

    main.cpp: use triangle function to model

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <algorithm>
 #include <cmath>
 #include <concepts>
+#include <print>
 
 #include "geometry.h"
 #include "model.h"
 #include "tgaimage.h"
 
 namespace {
-constexpr auto width  = 200;
-constexpr auto height = 200;
+constexpr auto width  = 800;
+constexpr auto height = 800;
 
-const auto white  = TGAColor(255, 255, 255, 255);
-const auto green  = TGAColor(0, 255, 0, 255);
-const auto red    = TGAColor(255, 0, 0, 255);
-const auto blue   = TGAColor(64, 128, 255, 255);
-const auto yellow = TGAColor(255, 200, 0, 255);
+const auto white  = TGAColor{255, 255, 255, 255};
+const auto green  = TGAColor{0, 255, 0, 255};
+const auto red    = TGAColor{255, 0, 0, 255};
+const auto blue   = TGAColor{64, 128, 255, 255};
+const auto yellow = TGAColor{255, 200, 0, 255};
 struct Position2D {
     int x;
     int y;
@@ -91,38 +92,31 @@ auto main(int argc, char** argv) -> int {
     auto framebuffer = TGAImage(width, height, TGAImage::RGB);
 
     // triangle
+    /*
     const auto triangle0 = std::array<Position2D, 3>{Position2D(10, 70), Position2D(50, 160), Position2D(70, 80)};
     const auto triangle1 = std::array<Position2D, 3>{Position2D(180, 50), Position2D(150, 1), Position2D(70, 180)};
     const auto triangle2 = std::array<Position2D, 3>{Position2D(180, 150), Position2D(120, 160), Position2D(130, 180)};
     triangle(triangle0, framebuffer, red);
     triangle(triangle1, framebuffer, white);
     triangle(triangle2, framebuffer, green);
+    */
 
     // load model
-    /*
     if(argc != 2) {
         std::println(stderr, "Usage: {} path/to/model.obj", argv[0]);
         return 1;
     }
 
-    auto model       = Model(argv[1]);
-    auto framebuffer = TGAImage(width, height, TGAImage::RGB);
+    auto model = Model(argv[1]);
 
     for(auto i = 0; i < model.nfaces(); i++) {
-        auto posa = project(model.vert(i, 0));
-        auto posb = project(model.vert(i, 1));
-        auto posc = project(model.vert(i, 2));
-        line(posa, posb, framebuffer, red);
-        line(posb, posc, framebuffer, red);
-        line(posc, posa, framebuffer, red);
+        auto posa  = project(model.vert(i, 0));
+        auto posb  = project(model.vert(i, 1));
+        auto posc  = project(model.vert(i, 2));
+        auto color = TGAColor(std::rand() % 255, std::rand() % 255, std::rand() % 255, std::rand() % 255);
+        triangle(std::array{posa, posb, posc}, framebuffer, color);
     }
 
-    for(auto i = 0; i < model.nverts(); i++) {
-        auto v = model.vert(i);
-        auto p = project(v);
-        framebuffer.set(p.x, p.y, white);
-    }
-    */
     framebuffer.flip_vertically();
 
     framebuffer.write_tga_file("output.tga");
