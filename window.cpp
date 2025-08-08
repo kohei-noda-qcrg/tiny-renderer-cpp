cbcf9e051652e336fe6bcfb928a40fc9f43a5499
Kohei Noda <ponserviceregist@gmail.com>
2025-08-08 14:58:15 +0900

    window.cpp: add FPS counter, Timer and frame count

--- window.cpp
+++ window.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <print>
 
 #include <GL/gl.h>
@@ -14,7 +15,7 @@ auto last_x      = 3.0;
 auto last_y      = 0.0;
 auto eye         = Vec3d(last_x, last_y, 3);
 auto is_dragging = false;
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+void mouse_button_callback(GLFWwindow* window, int button, int action, int /* mods */) {
     if(button == GLFW_MOUSE_BUTTON_LEFT) {
         if(action == GLFW_PRESS) {
             is_dragging = true;
@@ -24,7 +25,7 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
         }
     }
 }
-void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
+void cursor_position_callback(GLFWwindow* /* window */, double xpos, double ypos) {
     if(is_dragging) {
         double dx = xpos - last_x;
         double dy = ypos - last_y;
@@ -39,11 +40,40 @@ void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
         last_y = ypos;
     }
 }
+struct FPS_Counter {
+    FPS_Counter() : last_time(glfwGetTime()), frame_count(0), fps_(0) {};
+    auto update() -> void {
+        frame_count++;
+        const auto current_time = glfwGetTime();
+        if(current_time - last_time > period) {
+            fps_        = frame_count / (current_time - last_time);
+            frame_count = 0;
+            last_time   = current_time;
+        }
+    }
+    auto fps() -> double {
+        return fps_;
+    }
 
+  private:
+    const double period = 0.5;
+    double       last_time;
+    size_t       frame_count;
+    double       fps_;
+};
+struct Timer {
+    Timer() : start(std::chrono::steady_clock::now()), current(std::chrono::steady_clock::now()) {}
+    auto now() -> void { current = std::chrono::steady_clock::now(); }
+    auto duration() -> std::string { return std::format("{}", std::chrono::hh_mm_ss(std::chrono::duration_cast<std::chrono::milliseconds>(current - start))); }
+
+  private:
+    std::chrono::time_point<std::chrono::steady_clock> start, current;
+};
 } // namespace
 
 auto main(const int argc, const char* argv[]) -> int {
 
+    auto timer = Timer();
     if(argc != 2) {
         std::println(stderr, "Usage: {} path/to/model.obj", argv[0]);
         return 1;
@@ -97,13 +127,16 @@ auto main(const int argc, const char* argv[]) -> int {
     glViewport(0, 0, width, height);
     glClearColor(1.0, 1.0, 1.0, 0.0);
 
-    auto x = 0;
+    auto frame_count = 0;
+    auto fps_counter = FPS_Counter();
     while(glfwWindowShouldClose(window) == GL_FALSE) {
         std::fill(zbuffer.begin(), zbuffer.end(), std::numeric_limits<double>::max());
         image.fill(0);
-        std::print("\r{} times rendered", x);
+        fps_counter.update();
+        timer.now();
+        std::print("\rFPS {:.1f}, {}, {} times rendered", fps_counter.fps(), timer.duration(), frame_count);
         std::fflush(stdout);
-        x++;
+        frame_count++;
         paint_diffuse_texture_with_eye<gl::Shader>(eye, zbuffer, image, model, width, height);
 
         glTexImage2D(GL_TEXTURE_2D, 0, internal_format, image.get_width(), image.get_height(), 0, format, GL_UNSIGNED_BYTE, image.buffer());
