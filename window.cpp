40d7bcaa8f2162a4df52668653268dc9c1dead9b
Kohei Noda <ponserviceregist@gmail.com>
2025-08-08 10:02:18 +0900

    window.cpp: rotate 90°

--- window.cpp
+++ window.cpp
@@ -11,7 +11,19 @@ constexpr auto width  = 800;
 constexpr auto height = 800;
 } // namespace
 
-auto main() -> int {
+auto main(const int argc, const char* argv[]) -> int {
+
+    if(argc != 2) {
+        std::println(stderr, "Usage: {} path/to/model.obj", argv[0]);
+        return 1;
+    }
+
+    auto image   = TGAImage(width, height, TGAImage::RGBA);
+    auto zbuffer = std::vector<double>(width * height, std::numeric_limits<double>::max());
+    auto model   = Model(argv[1]);
+    if(!model.load_diffusemap(argv[1])) {
+        return 1;
+    }
 
     if(glfwInit() == GL_FALSE) {
         std::println(stderr, "failed to init glfw");
@@ -26,11 +38,6 @@ auto main() -> int {
 
     glfwMakeContextCurrent(window);
 
-    auto image = TGAImage();
-    if(!image.read_tga_file("output.tga")) {
-        std::println(stderr, "failed to read tga file");
-        return 1;
-    }
     auto format          = GL_RGBA;
     auto internal_format = GL_RGBA;
     switch(image.get_format()) {
@@ -52,27 +59,35 @@ auto main() -> int {
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, internal_format, image.get_width(), image.get_height(), 0, format, GL_UNSIGNED_BYTE, image.buffer());
-
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glViewport(0, 0, width, height);
     glClearColor(1.0, 1.0, 1.0, 0.0);
 
+    auto x = 0;
     while(glfwWindowShouldClose(window) == GL_FALSE) {
+        x++;
+        std::fill(zbuffer.begin(), zbuffer.end(), std::numeric_limits<double>::max());
+        image.fill(0);
+        std::print("\rx: {}", x);
+        std::fflush(stdout);
+        const auto eye = Vec3d(x % 64, 0, 3);
+        paint_diffuse_texture_with_eye(eye, zbuffer, image, model, width, height);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, internal_format, image.get_width(), image.get_height(), 0, format, GL_UNSIGNED_BYTE, image.buffer());
         glClear(GL_COLOR_BUFFER_BIT);
 
         glEnable(GL_TEXTURE_2D);
         glBindTexture(GL_TEXTURE_2D, texture);
 
         glBegin(GL_QUADS);
-        glTexCoord2f(0.0f, 1.0f);
-        glVertex2f(-1.0f, -1.0f);
-        glTexCoord2f(1.0f, 1.0f);
-        glVertex2f(1.0f, -1.0f);
-        glTexCoord2f(1.0f, 0.0f);
-        glVertex2f(1.0f, 1.0f);
         glTexCoord2f(0.0f, 0.0f);
+        glVertex2f(-1.0f, -1.0f);
+        glTexCoord2f(1.0f, 0.0f);
+        glVertex2f(1.0f, -1.0f);
+        glTexCoord2f(1.0f, 1.0f);
+        glVertex2f(1.0f, 1.0f);
+        glTexCoord2f(0.0f, 1.0f);
         glVertex2f(-1.0f, 1.0f);
         glEnd();
 
